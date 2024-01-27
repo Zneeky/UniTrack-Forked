dcafee8d5358908b41115623683f827bfefe5f6d
IvelinP30 <133099999+IvelinP30@users.noreply.github.com>
2024-01-27 22:14:59 +0200

    removed unnecessary code

--- UniTrackUI/src/app/chat/chat/chat.component.ts
+++ UniTrackUI/src/app/chat/chat/chat.component.ts
@@ -6,7 +6,6 @@ import { StudentProfile } from 'src/app/shared/models/student-profile';
 import { UserResult } from 'src/app/shared/models/user-result';
 import { ChatRequestsService } from 'src/app/shared/services/chat.requests.service';
 import { ChatService } from 'src/app/shared/services/chat.service';
-import { TeachersService } from 'src/app/shared/services/teachers.service';
 
 interface ChatEntry {
   personName: string;
@@ -38,6 +37,7 @@ export class ChatComponent {
 
 
   ngOnInit(): void {
+
     this.chatRequestsService
       .getContacts()
       .pipe(tap((contacts) => (this.contactsData = contacts)))
@@ -57,6 +57,7 @@ export class ChatComponent {
   this.chatRequestsService.getMessageHistory() 
   .pipe(tap((chat) => (this.chatsHistory = chat)))
   .subscribe();
+  
 }
 
   getMessageHistory(){
@@ -64,40 +65,6 @@ export class ChatComponent {
     .pipe(tap((chat) => (this.chatsHistory = chat)))
     .subscribe();
   }
-  // people = [
-  //   { name: 'Alice', avatar: 'path/to/avatar1.jpg' },
-  //   { name: 'Bob', avatar: 'path/to/avatar2.jpg' },
-  //   { name: 'Charlie', avatar: 'path/to/avatar3.jpg' },
-  //   { name: 'David', avatar: 'path/to/avatar4.jpg' },
-  //   { name: 'Eva', avatar: 'path/to/avatar5.jpg' },
-  //   { name: 'Frank', avatar: 'path/to/avatar6.jpg' },
-  //   { name: 'Grace', avatar: 'path/to/avatar7.jpg' },
-  //   { name: 'Henry', avatar: 'path/to/avatar8.jpg' },
-  //   { name: 'Ivy', avatar: 'path/to/avatar9.jpg' },
-  //   { name: 'Jack', avatar: 'path/to/avatar10.jpg' },
-  //   { name: 'Katherine', avatar: 'path/to/avatar11.jpg' },
-  //   { name: 'Leo', avatar: 'path/to/avatar12.jpg' },
-  //   { name: 'Mia', avatar: 'path/to/avatar13.jpg' },
-  //   { name: 'Nathan', avatar: 'path/to/avatar14.jpg' },
-  //   { name: 'Olivia', avatar: 'path/to/avatar15.jpg' },
-  //   { name: 'Peter', avatar: 'path/to/avatar16.jpg' },
-  //   { name: 'Quinn', avatar: 'path/to/avatar17.jpg' },
-  //   { name: 'Rachel', avatar: 'path/to/avatar18.jpg' },
-  //   { name: 'Samuel', avatar: 'path/to/avatar19.jpg' },
-  //   { name: 'Tara', avatar: 'path/to/avatar20.jpg' },
-  //   { name: 'Ulysses', avatar: 'path/to/avatar21.jpg' },
-  //   { name: 'Victoria', avatar: 'path/to/avatar22.jpg' },
-  //   { name: 'Walter', avatar: 'path/to/avatar23.jpg' },
-  //   { name: 'Xena', avatar: 'path/to/avatar24.jpg' },
-  //   { name: 'Yasmine', avatar: 'path/to/avatar25.jpg' },
-  //   { name: 'Zane', avatar: 'path/to/avatar26.jpg' },
-  //   { name: 'Anna', avatar: 'path/to/avatar27.jpg' },
-  //   { name: 'Benjamin', avatar: 'path/to/avatar28.jpg' },
-  //   { name: 'Chloe', avatar: 'path/to/avatar29.jpg' },
-  //   { name: 'Dylan', avatar: 'path/to/avatar30.jpg' }
-  // ];
-
-
 
   selectedChat: ChatEntry = {
     personName: '',
@@ -105,7 +72,6 @@ export class ChatComponent {
     receiverUserId: "",
   };
 
-  // topRowTranslation = '0'; // Translation for the top row sliding effect
   newMessage: string = '';
 
   selectChat(personName: string, userId: string): void {
@@ -115,7 +81,6 @@ export class ChatComponent {
       receiverUserId: userId
     };
   
-    // Call getMessagesInChat to load the message history for the selected chat
     this.chatRequestsService.getMessagesInChat(userId).subscribe(
       (messageResults: MessageResult[]) => {
         this.selectedChat.messages = messageResults.map((msgResult) => {
@@ -127,81 +92,27 @@ export class ChatComponent {
       },
       (error) => {
         console.error('Error loading messages:', error);
-        // Handle error (e.g., show an error message to the user)
       }
     );
   }
 
 
-  // getLastMessage(firstName: string, lastName: string): string {
-  //   // Find the chat entry in chatsHistory based on the person's name
-  //   const chatEntry = this.chatsHistory.find(entry => entry.personName === personName);
-
-  //   if (chatEntry) {
-  //     // Get the last message from the messages array
-  //     const lastMessage = chatEntry.messages[chatEntry.messages.length - 1];
-  //     return lastMessage ? lastMessage.text : 'No messages';
-  //   } else {
-  //     return 'No messages';
-  //   }
-  // }
-
-  
-  // generateFakeMessages(count: number): Message[] {
-  //   const messages: Message[] = [];
-  //   for (let i = 0; i < count; i++) {
-  //     let text = '';
-  //     switch (i % 6) {
-  //       case 0:
-  //         text = 'Hi there!';
-  //         break;
-  //       case 1:
-  //         text = 'Hello!';
-  //         break;
-  //       case 2:
-  //         text = 'How are you?';
-  //         break;
-  //       case 3:
-  //         text = 'Good!, You?';
-  //         break;
-  //       case 4:
-  //         text = 'Me too';
-  //         break;
-  //       case 5:
-  //         text = 'Ok, bye';
-  //         break;
-  //       default:
-  //         text = 'Default message';
-  //         break;
-  //     }
-
-  //     messages.push({
-  //       text: text,
-  //       sentBy: i % 2 === 0 ? 'me' : 'friend',
-  //     });
-  //   }
-  //   return messages;
-  // }
-
   sendMessage(): void {
-    if (!this.newMessage.trim()) return; // Prevent sending empty messages
+    if (!this.newMessage.trim()) return; 
   
     this.chatService.sendMessageToUser(this.selectedChat.receiverUserId, this.newMessage).then(() => {
       const newMessage: Message = {
         text: this.newMessage,
         sentBy: 'me',
       };
-  
-      // Add the new message to the selectedChat messages array
       this.selectedChat.messages.push(newMessage);
   
-      // Clear the input after sending the message
       this.newMessage = '';
   
       console.log('Message sent');
     }).catch(error => {
       console.error('Error sending message:', error);
-      // Handle error (e.g., show an error message to the user)
+
     });
   }
 }
